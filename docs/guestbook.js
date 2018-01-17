808fc842affed46bed58b23ee554634b0028b771
ptaube <ptaube@users.noreply.github.com>
2018-01-17 13:00:27 +0100

    Using POST now instead of PUT

--- docs/guestbook.js
+++ docs/guestbook.js
@@ -15,7 +15,7 @@ const guestbook = {
   add(name, email, comment) {
     console.log('Sending', name, email, comment)
     return $.ajax({
-      type: 'PUT',
+      type: 'POST',
       url: `${apiUrl}/entries`,
       contentType: 'application/json; charset=utf-8',
       data: JSON.stringify({
